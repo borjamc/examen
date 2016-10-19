810a75f37c1b589512f1034202f372ced3600410
Borja Molina Catalan <bomoca@floridauniversitaria.es>
2016-10-19 12:30:00 +0200

    first commit

---
+++ Examen/Animal.php
@@ -0,0 +1,38 @@
+<?php 
+
+Class Animal
+
+{
+
+public $animal="Perro";
+public $numPatas=4;
+
+public function getanimal(){
+return $this->animal;
+
+}
+
+public function getnumPatas(){
+return $this->numPatas;
+
+}
+
+public function setanimal($ani){
+$this->animal=$ani;
+
+}
+
+public function setnumPatas($patas){
+$this->numPatas=$patas;
+
+}
+
+
+
+
+}
+
+
+
+
+ ?>
---
+++ Examen/CrearAnimal.php
@@ -0,0 +1,19 @@
+<?php 
+
+include 'Animal.php';
+
+public $animal1="cienpies";
+public $animal2="araña";
+
+if ($animal1="cienpies"){
+$numPatas=100;
+}
+if ($animal2="araña"){
+$numPatas=8	
+}
+echo "$numPatas";
+
+
+
+
+ ?>
